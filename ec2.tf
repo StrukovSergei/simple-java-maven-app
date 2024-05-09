b5c24327178d794d282944ab1535a6b6ad32b01d
sergei.strukov <strukov.sergei.999@gmail.com>
2024-05-09 18:14:34 +0300

    changed main.yml

---
+++ ec2.tf
@@ -0,0 +1,33 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+provider "aws" {
+  region  = "eu-west-3"
+  profile = "default"
+
+}
+
+resource "aws_instance" "instance_from_registry_sec_group" {
+  ami                    = "ami-00ac45f3035ff009e"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-0dae6c351255f9e7e"]
+  key_name               = "app-ssh-key"
+  tags = {
+    Name = "java-app"
+  }
+    user_data = <<-EOF
+              #!/bin/bash
+                sudo apt-get update
+                sudo apt-get install docker.io -y
+                sudo systemctl start docker
+                sudo systemctl enable docker
+                sudo docker run -p 80:8000 -d strukovsergei/test-rep:latest
+              EOF
+}
+
